e3b1a91a87d362c384634db306cfbe692673adb8
Loya Altangerel <luvsaa@amazon.com>
2017-09-30 22:02:51 -0700

    application.java and build gradle

--- src/main/java/mn/mosc/project/Application.java
+++ src/main/java/mn/mosc/project/Application.java
@@ -1,7 +1,17 @@
 package mn.mosc.project;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import mn.mosc.project.domain.repository.authorization.UserRepository;
+import mn.mosc.project.domain.repository.inventory.ProductRepository;
+import mn.mosc.project.domain.repository.order.TransferRequestDetailRepository;
+import mn.mosc.project.domain.repository.order.TransferRequestRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * created by ubulgan on 9/30/17
@@ -9,6 +19,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        try (InputStream inputStream = Application.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties appProperties = new Properties();
+            appProperties.load(inputStream);
+
+            System.setProperty("aws.accessKeyId", appProperties.getProperty("accessKeyId"));
+            System.setProperty("aws.secretKey", appProperties.getProperty("secretKey"));
+            System.setProperty("aws.region", appProperties.getProperty("region"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SpringApplication.run(Application.class, args);
     }
 }
