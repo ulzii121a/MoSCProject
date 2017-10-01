46e06f1c08fbe36a1a3f0c8ce9772be1e563a91c
Amaraa <amarbayar.amarsanaa@gmail.com>
2017-09-30 23:52:38 -0700

    Importing the library

--- src/main/java/mn/mosc/project/Application.java
+++ src/main/java/mn/mosc/project/Application.java
@@ -2,6 +2,7 @@ package mn.mosc.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import java.io.InputStream;
 import java.util.Properties;
