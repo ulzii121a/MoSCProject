562802d26c1e35b2b1f15739d623783ae66ed921
Amaraa <amarbayar.amarsanaa@gmail.com>
2017-10-01 00:53:19 -0700

    App class update

--- src/main/java/mn/mosc/project/Application.java
+++ src/main/java/mn/mosc/project/Application.java
@@ -2,28 +2,21 @@ package mn.mosc.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * created by ubulgan on 9/30/17
  */
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
     public static void main(String[] args) {
-        try (InputStream inputStream = Application.class.getClassLoader().getResourceAsStream("app.properties")) {
-            Properties appProperties = new Properties();
-            appProperties.load(inputStream);
-
-            System.setProperty("aws.accessKeyId", appProperties.getProperty("accessKeyId"));
-            System.setProperty("aws.secretKey", appProperties.getProperty("secretKey"));
-            System.setProperty("aws.region", appProperties.getProperty("region"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         SpringApplication.run(Application.class, args);
     }
 }
