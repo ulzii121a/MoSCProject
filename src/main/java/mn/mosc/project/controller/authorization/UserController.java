386ae0b751f46b7516f0aa51187fb7ab20386412
Loya Altangerel <luvsaa@amazon.com>
2017-10-16 22:33:04 -0700

    Removed line from User Controller class

--- src/main/java/mn/mosc/project/controller/authorization/UserController.java
+++ src/main/java/mn/mosc/project/controller/authorization/UserController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 /**
  * created by ubulgan on 9/30/17
  */
